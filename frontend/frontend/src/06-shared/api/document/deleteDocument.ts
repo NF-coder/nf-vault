25cdf12a59d8524d94fd805aa0702cee27bec258
NF-coder <resetnikovs72@gmail.com>
2026-07-06 11:23:12 +0300

    feat(api): add api versioning constant

--- frontend/frontend/src/06-shared/api/document/deleteDocument.ts
+++ frontend/frontend/src/06-shared/api/document/deleteDocument.ts
@@ -1,4 +1,5 @@
 import { request } from "../request";
+import { API_V1_PATH } from "../config";
 
 type deleteDocumentReq = {
   docId: number
@@ -8,7 +9,7 @@ type deleteDocumentRes = {}
 export const deleteDocument = async ({
   docId,
 }: deleteDocumentReq): Promise<deleteDocumentRes> => {
-  await request(`/api/document/${docId}`, {
+  await request(`${API_V1_PATH}/document/${docId}`, {
     method: "DELETE",
   });
   return {};
