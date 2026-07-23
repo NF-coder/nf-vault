00e6fe1fe81dd8f5cf84b553e65f9d376e149f4c
NF-coder <resetnikovs72@gmail.com>
2026-07-24 00:07:52 +0300

    refactor: remove deprecated pm code

--- frontend/frontend/src/03-widgets/editor/lib/prosemirror/config/constants.ts
+++
@@ -1,8 +0,0 @@
-import { Schema } from "prosemirror-model";
-import { schema as markdownSchema } from "prosemirror-markdown";
-import { markerNodeSpec } from "./markers/inlineMarkerSpec";
-
-export const schema = new Schema({
-  nodes: markdownSchema.spec.nodes.addToEnd("marker", markerNodeSpec),
-  marks: markdownSchema.spec.marks,
-});
